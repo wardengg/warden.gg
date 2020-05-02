d2396b55ff7aa1c2a7560a814a01eb935221144c
gabefraser <gabe@gabefraser.com>
2020-05-02 19:20:13 +1000

    Update: Start on locations

---
+++ src/components/LocationSection.vue
@@ -0,0 +1,14 @@
+<template>
+    <section class="location-section">
+        <div class="wrap">
+            <h1 class="section-header">Locations</h1>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'LocationSection',
+
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,20 @@
     <div class="home">
         <VideoBanner/>
         <GameserverTypeSection/>
+        <LocationSection/>
     </div>
 </template>
 
 <script>
 import VideoBanner from '../components/VideoBanner.vue'
 import GameserverTypeSection from '../components/GameserverTypeSection.vue'
+import LocationSection from '../components/LocationSection.vue'
 export default {
     name: 'Home',
     components:{
         VideoBanner,
-        GameserverTypeSection
+        GameserverTypeSection,
+        LocationSection
     }
 }
 </script>

7a440ff0d12f9cb346f265dbf33120b0bfa3ace3
gabefraser <gabe@gabefraser.com>
2020-05-05 18:45:09 +1000

    Update: Restyle the defaults for Why Warden

--- src/components/blocks/WhyWardenSection.vue
+++ src/components/blocks/WhyWardenSection.vue
@@ -29,37 +29,37 @@ export default {
             visible: false,
             whywarden: [
                 {
-                    icon: '/static/images/panel.svg',
+                    icon: '/static/images/panel-white.svg',
                     title: 'Extensible Game Panel',
                     description: "We're constantly upgrading and adding new features to the game panel so you can get the most out of your servers!"
                 },
                 {
-                    icon: '/static/images/support.svg',
+                    icon: '/static/images/support-white.svg',
                     title: 'Premium Support',
                     description: "Bad things can sometimes be inevitable, but that's why we're always going to be there for you, regardless of the situation."
                 },
                 {
-                    icon: '/static/images/ftp.svg',
+                    icon: '/static/images/ftp-white.svg',
                     title: 'Full Server Access',
                     description: "You're granted complete access to your servers, allowing you to reach for the sky!"
                 },
                 {
-                    icon: '/static/images/firewall.svg',
+                    icon: '/static/images/firewall-white.svg',
                     title: 'DDoS Protection',
                     description: "Our network is protected by a firewall that is constantly adapting itself to ensure a playable experience."
                 },
                 {
-                    icon: '/static/images/ethernet.svg',
+                    icon: '/static/images/ethernet-white.svg',
                     title: '1Gbps Network',
                     description: "Every server is equipped with a high-speed link allowing for an uninterrupted connection."
                 },
                 {
-                    icon: '/static/images/clock.svg',
+                    icon: '/static/images/clock-white.svg',
                     title: 'Always Online',
                     description: "Never miss out on the fun. Our servers are online 24/7. Play till you pass out at your desk, we encourage it."
                 },
                 {
-                    icon: '/static/images/meter.svg',
+                    icon: '/static/images/meter-white.svg',
                     title: 'Powerful Architecture',
                     description: "Our servers are powered by the latest Xeon processors and super-fast SSD drives! Oh yeah, feel the speed baby."
                 },
